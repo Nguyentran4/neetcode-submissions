933b0cf815db6c09d1503f5d0a0284fb20fe1b80
Nguyen Tran <bachtran700@gmail.com>
2026-06-17 20:24:26 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+
+        // non-increasing stack
+        Stack<Integer> stack = new Stack<>(); //stores indices
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                res[prevDay] = i - prevDay;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
