9d2dba3e8bdd61f5df0891fe5b7918267911b8c1
Nguyen Tran <bachtran700@gmail.com>
2026-06-21 21:45:05 -0400

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        HashMap<Character, Integer> lastIndex = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            lastIndex.put(s.charAt(i), i);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        int start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            end = Math.max(end, lastIndex.get(s.charAt(i)));
+
+            if(i == end){
+                res.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return res;
+
+    } //Expand window using last occurrence;
+    //close partition when index hits farthest last index of all seen characters.
+}
